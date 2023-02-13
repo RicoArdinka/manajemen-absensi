d810692825e4dd33da59bd7062d1b629a664d595
rico ardinka <ricosepta1014@gmail.com>
2023-02-13 21:43:42 +0700

    dikit

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'halaman/tampil';
-$route['404_override'] = 'orang/nyasar';
-$route['translate_uri_dashes'] = FALSE;
+$route['default_controller'] = 'halaman/tampil'; // ini menampilkan halaman pertama kali
+$route['404_override'] = 'orang/nyasar'; //membuat error sendiri
+$route['translate_uri_dashes'] = FALSE; //penamaan url untuk dash(-)
