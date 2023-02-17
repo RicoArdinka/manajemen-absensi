93dd647c79fc9e0d206c9519457816327321af9d
rico ardinka <ricosepta1014@gmail.com>
2023-02-17 20:41:26 +0700

    prepare template dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -10,15 +10,12 @@ class Dashboard extends CI_Controller {
     
 	public function index()
 	{
-
-		print_r($this->session->userdata());
 		// $this->load->database(); sudah diwakilkan oleh autoload.php
 		// $this->load->model('Karyawan_model');
 		$data['title'] = "Dashboard";
 
-		$this->load->view('template/home/header', $data);
-		$this->load->view('pages/dashboard', $data);
-		$this->load->view('template/home/footer', $data);
+		$this->load->view('template/dashboard/header', $data);
+		$this->load->view('template/dashboard/body', $data);
 
 	}
 }
---
+++ application/views/template/dashboard/body.php
@@ -0,0 +1,29 @@
+<div class="dashboard">
+    <!-- ini tampilan untuk sebelah kiri -->
+    <nav id="sidebar" class="bg-dark">
+        <ul class="menu">
+            <li><?= anchor('dashboard', 'Dashboard'); ?></li>
+            <li><?= anchor('dashboard/karyawan', 'Karyawan'); ?></li>
+            <li><?= anchor('dashboard/gaji-dan-tunjangan', 'Gaji & Tunjangan'); ?></li>
+            <li><?= anchor('dashboard/absensi', 'Absensi'); ?></li>
+            <li><?= anchor('dashboard/posisi', 'Posisi'); ?></li>
+            <li><?= anchor('dashboard/departemen', 'Departemen'); ?></li>
+            <li><?= anchor('logout', 'Logout'); ?></li>
+        </ul>
+    </nav>
+
+    <!-- ini tampilan untuk sebelah kanan -->
+    <div id="content">
+        <nav class="navbar navbar-expand bg-light">
+            <button type="button" id="sidebarCollapse" class="btn btn-outline-dark">
+                <i class="fa fa-align-justify"></i>
+            </button>
+
+            <a class="navbar-brand logo-dashboard" href="#">COLCOM</a>
+        </nav>
+
+        <div class="container-fluid">
+            <!-- ini adalah konten -->
+        </div>
+    </div>
+</div>
