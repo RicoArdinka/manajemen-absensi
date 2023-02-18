bd344a4c104a4e4ca554c21c76e20896c8353983
rico ardinka <ricosepta1014@gmail.com>
2023-02-18 12:35:20 +0700

    setup jquery and fontawesome

--- application/views/template/dashboard/header.php
+++ application/views/template/dashboard/header.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-4.3.1-dist/css/bootstrap.min.css');?>"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fontawesome-free-5.12.1-web/css/all.min.css'); ?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>"/>
 </head>
 <body>
