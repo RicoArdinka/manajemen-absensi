283183f90eb6d0551eb03d3df335e51811d6412c
rico ardinka <ricosepta1014@gmail.com>
2023-02-19 14:08:26 +0700

    install malihu scrollbar

--- application/views/template/dashboard/header.php
+++ application/views/template/dashboard/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-4.3.1-dist/css/bootstrap.min.css');?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fontawesome-free-5.12.1-web/css/all.min.css'); ?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css'); ?>"/>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/halaman/dashboard.css'); ?>"/>
 
 </head>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 //akan meload semua document yg ada
 $("document").ready(function () {
 	// jika ini diklik maka
+
+	$("#sidebar").mCustomScrollbar({
+		theme: "minimal",
+	});
+
 	$("#sidebarCollapse").click(function () {
 		$("#sidebar").toggleClass("tutup"); //jika belum ada class tutup maka dibuatkan / jika ada dihapus
 		$("#content").toggleClass("layarPenuh"); //jika belum ada class layar penuh maka dibuatkan / jika ada dihapus
